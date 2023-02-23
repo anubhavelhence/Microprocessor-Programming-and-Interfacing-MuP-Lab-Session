423a67796b947fcc1efe72c4c2787320e3ce5417
Prateek Kashyap <kashyapprateek13@gmail.com>
2023-02-23 08:55:34 +0530

    fix: provide simple comments to instruct

--- ex1.asm
+++ ex1.asm
@@ -1,17 +1,20 @@
 .model tiny
+
+; START FILLING DATA ---
 .data
 data1 db '[',']','*'
 data2 dw 9876h, 'a', 'A', '0'
 data3 dw 0fefeh
 array dw 01,02,03,04,05,06,07,08,09
 
+; START FILLING CODE ---
 .code
 .startup
-    mov BX,data2
-    mov CX,data3
-    mov data1,BL
-    mov DL,data1
-    mov DI,0002h
-    mov AX,array[DI]
+    mov BX,data2        ; moves data2 (9876) to BX
+    mov CX,data3        ; moves data3 (FEFE) to CX
+    mov data1,BL        ; moves BL (76: lower in BX) to data1
+    mov DL,data1        ; moves data1 (76) to DL
+    mov DI,0002h        ; moves 02 to DI immediately
+    mov AX,array[DI]    ; moves [array + DI] (here 02 only) to AX
 .exit
 end
