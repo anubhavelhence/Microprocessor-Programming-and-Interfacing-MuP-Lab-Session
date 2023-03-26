28275661d060052810041016fc29fb24fdf6042f
Anubhav Elhence <elhenceanubhav@gmail.com>
2023-03-27 03:17:06 +0530

    week9c1 update

--- week9_c1.asm
+++ week9_c1.asm
@@ -4,13 +4,20 @@
 .code
 .startup
 
-    MOV AH, 00H
-    MOV AL, 03h
-    INT 10H
-    ; mov ah,07h
-    ; x1: int 21h
-    ; cmp al,'%'
-    ; jnz x1
+    mov ah, 0
+    Mov al, 12h
+    int 10h ; set graphics video mode.
+
+    mov ah, 0ch
+    mov al, 00001100b
+    mov cx, 10
+    mov dx, 20
+    int 10h ; set pixel.
+
+    mov ah,07h
+    x1: int 21h
+    cmp al,'%'
+    jnz x1
 
 
 
